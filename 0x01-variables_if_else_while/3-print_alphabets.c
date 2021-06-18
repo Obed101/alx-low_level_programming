e3ae94c734d87f0e5b7677b6884fae1e740d8dac
obed101 <obedamoako92@gmail.com>
2021-06-18 13:59:03 -0700

    ok

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+        char alphabets = 'a';
+	char caps = 'A';
+	while (alphabets <= 'z')
+{
+       putchar(alphabets);
+        alphabets++;
+}
+	while (caps <= 'Z')
+	{	putchar(caps);
+		caps++;
+	}
+putchar('\n');
+return (0);
+}
