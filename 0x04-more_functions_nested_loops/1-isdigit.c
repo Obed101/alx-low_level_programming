c6f36f2c587b5d9e981afced0d5fce517507ea21
obed101 <obedamoako92@gmail.com>
2021-06-24 10:46:56 -0700

    digs

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isdigit - check the code for ALX School students.
+ *
+ * @c: Character to pass to _islower function
+ *
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+        if ((c >= 48) && (c <= 57))
+        {
+                return (1);
+        }
+                else
+        {
+                return (0);
+        }
+}
