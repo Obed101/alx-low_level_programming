1f895d76368a1e7f7d4b3fddab7b763eaad6bded
obed101 <obedamoako92@gmail.com>
2021-06-24 05:44:21 -0700

    comment

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
         i = 0;
 
-        while (i < 10) /*this line is causing infinite loops*/
+        while (i < 10)
         {
-                putchar(i);
-        }
+                putchar(i); /*this line is causing infinite loops*/
+	}
 
         printf("Infinite loop avoided! \\o/\n");
 
