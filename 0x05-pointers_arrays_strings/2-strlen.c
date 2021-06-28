f0be897282b806591469ab04c60f77d719e8ea0c
obed101 <obedamoako92@gmail.com>
2021-06-28 08:25:51 -0700

    printer prints the lenght of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,16 +2,17 @@
 /**
  *_strlen - displays the lenght of a string.
  *
+ *@s: A variable that reps string pointer
  *Return: Always success
  */
 int _strlen(char *s)
 {
 	int lnt = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		lnt++;
 		s++;
 	}
-	return(lnt);
+	return (lnt);
 }
