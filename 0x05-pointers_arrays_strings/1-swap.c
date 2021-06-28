2dc1d59a0497dd564e01fd045b635be7a605afd9
obed101 <obedamoako92@gmail.com>
2021-06-28 06:23:12 -0700

    swap them

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps the variables stated
+ *
+ * @a: is an integer
+ * @b: another integer
+ *
+ * Return: Always Success
+ */
+void swap_int(int *a, int *b)
+{
+	swap_int(a, b);
+	return(0);
+}
