30dde73f691e603edbc1eeba691c6404afc07fcc
obed101 <obedamoako92@gmail.com>
2021-07-05 13:37:18 -0700

    correction for betty

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,13 @@
 */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0' ; i++)
+	while (*s)
 	{
-		if (s[i] == c)
-		return (&s[i]);
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
 	}
-	return (0);/*returns a char type*/
+	return (!c ? s : NULL);
 }
