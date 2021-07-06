0700a05abee09f2ae421805b1c75b76c6eb79acb
obed101 <obedamoako92@gmail.com>
2021-07-06 04:00:54 -0700

    double the psswd

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * set_string - set  the value of a pointer to a char
+ *
+ * @s: pointer to pinter
+ * @to: string to set
+ *
+ *
+ * Return: a char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
