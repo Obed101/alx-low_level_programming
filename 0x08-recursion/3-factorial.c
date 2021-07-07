a0494186666da5547875d3285e5464aaacaefcf3
obed101 <obedamoako92@gmail.com>
2021-07-07 02:37:05 -0700

    factorial1

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
+ *factorial - returns the factors multiplied
  *
+ *@n: number/integer
  *
- *
- *
- *
+ *return: 0 or -1
  */
 int factorial(int n)
 {
 	if (n < 0)
-		return (0);
+		return (-1);
 
 	if (n = 0)
 		return (n * factorial(n));
