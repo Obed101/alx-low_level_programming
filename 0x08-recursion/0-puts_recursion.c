72cebd378692d013d049f4183b6047bdbd1a23cc
obed101 <obedamoako92@gmail.com>
2021-07-06 23:54:41 -0700

    added header and readme

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_puts_recursion - prints a string in recursion
+ *
+ *@s - string
+ *
+ *
+ *
+ */
+void _puts_recursion(char *s)
+{
+
+	return ();
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+_recursion_ is done **globally**
