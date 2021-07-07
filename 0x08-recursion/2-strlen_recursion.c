c8068e4d174337f395631f8c52d6b8cdf129d6d3
obed101 <obedamoako92@gmail.com>
2021-07-07 11:17:58 -0700

    edit to count str

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
+
 /**
- *_strlen_recursion - returns lenght of string
- *len: integer var
- *@s: string
- *return: lenght of string.
+ * _strlen_recursion - function returns the length of a string.
+ * @s: The string.
+ * Return: length
  */
 int _strlen_recursion(char *s)
 {
-	int len;
+	int length = 0;
 
 	if (*s == 0)
 		return (0);
 
-	len = _strlen_recursion(s + 1);
+	length = _strlen_recursion(s + 1);
 
-	return (len + 1);
+	return (length + 1);
 }
