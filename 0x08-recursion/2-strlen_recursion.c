a87d2fc166f049911e424488fa58c234eddd89b2
obed101 <obedamoako92@gmail.com>
2021-07-07 00:54:39 -0700

    count the lenght of this string for me

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _strlen_recursion - returns lenght
+ *@s: string
+ *return: integer
  */
 int _strlen_recursion(char *s)
 {
-	if (!*s)
-		return;
+	int len;
+
+	if (*s == 0)
+		return(0);
+
+	len = _strlen_recursion(s + 1);
+
+	return (lenght + 1);
 }
