00a392491634c112117e5428d84c9163afab283b
obed101 <obedamoako92@gmail.com>
2021-07-12 05:19:53 -0700

    duplicating every argument and print dup.

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,16 +5,14 @@
  *
  *@argc: counts the number of arguments
  *@argv: prints the argument strings, a pointer
- *@count: counts the number of argc
  *Return: 0 if successful
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	while (argc--)
-		{
-			printf("%s\n", *argv++);
-		}
-
+	{
+		printf("%s\n", *argv++);
+	}
 	return (0);
 }
