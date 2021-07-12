3cf9d01ccadbd303712eb0f9301ebe2bb78c4ae8
obed101 <obedamoako92@gmail.com>
2021-07-12 03:48:26 -0700

    a file that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - entry point
+ *argc: variable that counts the number of strings
+ *argv: variable, array, pointer, of strings
+ *exit: stops everything if success, print correct output
+ */
+
+int main(int argc, char **argv)
+{
+	while(argc--)
+	printf("%s\n", *argv++);
+
+	exit(EXIT_SUCCESS);
+	/*return(0);*/
+}
