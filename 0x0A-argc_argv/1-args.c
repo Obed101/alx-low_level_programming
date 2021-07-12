46dd9e02219e40c9f55fd6e97bc57dbf988a752e
obed101 <obedamoako92@gmail.com>
2021-07-12 04:30:43 -0700

    created a file whose output is its name

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,9 +4,8 @@
  * main - prints the number of arguments
  *
  * @argc: used to count arguments
- * @argv: not really used. but prints the arguments
- *
- * return: 0 if successful
+ * @argv: not really used. but prints the arguments of array, string
+ * Return: 0 if successful
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
