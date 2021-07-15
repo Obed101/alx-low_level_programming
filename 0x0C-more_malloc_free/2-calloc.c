96dff8d5b328d7dad896eee48bc4112945135e98
obed101 <obedamoako92@gmail.com>
2021-07-15 11:05:00 -0700

    calloc or malloc? choose

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,5 +3,19 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
+	unsigned int i;
+	char *ptr;
+	void *ok;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < (size * nmemb); i++)
+		ptr[i] = 0;
+	ok = ptr;
+	return (ok);
 }
