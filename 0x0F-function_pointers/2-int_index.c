e7fe872745b5b453b2b4cc5637307c696a821ea6
obed101 <obedamoako92@gmail.com>
2021-07-20 04:43:23 -0700

    this file looks for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * int_index - function that looks for the index of integer
+ * @array: the current array in use
+ * @cmp: the poinyter to the function
+ * @size: size of the array
+ *
+ * Return: the index (i)
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (!array || size <= 0 || !cmp)
+		return(-1);
+	for (i = 0; i < size; i++)
+	{
+		if(cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
