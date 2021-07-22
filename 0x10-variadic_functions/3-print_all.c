b507c7a668fd1d30aeaddd6f8643ef383ee5185a
obed101 <obedamoako92@gmail.com>
2021-07-22 12:55:37 -0700

    :new:

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - function that prints all args
+ * @format: the type
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	printf("%s", format);
+/**
+ * char - *separator = "";
+ * int i, j = 0;
+ * va_list valist;
+ *
+ * mytype choice[] = {'c', print_char},
+ * {'i', print_int},
+*/
+
+}
