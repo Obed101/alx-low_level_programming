68f28cb66155c34831a57f1b76447bba947f3ca7
obed101 <obedamoako92@gmail.com>
2021-07-29 11:24:16 -0700

    added a file that adds node to list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,7 @@
-#ifndef LISTS_H_
-#define LISTS_H_
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
 #include <stddef.h>
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 
 /**
  * struct list_s - singly linked list
@@ -16,12 +12,18 @@ void free_list(list_t *head);
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-#endif
+int _putchar(char);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* _LISTS_H_ */
