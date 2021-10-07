80925acff028dfb8e9944fae8553d705fe706fd9
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-10-07 18:14:33 +0100

    Documented the function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,9 @@
 #include "hash_tables.h"
 
 /**
-*/
+ * hash_table_create - a function that creates a hash table (as name implies) of size @size
+ *
+ * @size: The size of the hash table (the spaces allocated for nodes)
+ * Return: pointer to the hash table on success, else, null
+ */
 hash_table_t *hash_table_create(unsigned long int size);
