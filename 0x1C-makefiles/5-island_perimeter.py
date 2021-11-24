634cda712c753b4d1cb4ad56e5da48af1dba67c7
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-11-24 23:59:04 +0000

    changed the doc

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,7 +6,7 @@ def island_perimeter(grid):
 	"""Defiines island representing land and water with
     0 and 1 respectively
     """
-	    x, y = 0, 0
+	x, y = 0, 0
     for row in range(1, len(grid) - 1):
             for col in range(1, len(grid[row]) - 1):
                 if grid[row][col] == 1:
