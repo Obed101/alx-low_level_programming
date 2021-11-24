cb320b9022f4ebf3e7692ab0d936efb6bd5ed268
Obed Amoako <84608830+Obed101@users.noreply.github.com>
2021-11-24 23:46:45 +0000

    island perimeter rep

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+  #!/usr/bin/python3
+  """This module defines isand"""
+
+
+def island_perimeter(grid):
+	"""Defiines island representing land and water with
+    0 and 1 respectively
+    """
+	    x, y = 0, 0
+    for row in range(1, len(grid) - 1):
+            for col in range(1, len(grid[row]) - 1):
+                if grid[row][col] == 1:
+                    if grid[row][col - 1] == 0:
+                        y += 1
+                    if grid[row][col + 1] == 0:
+                        y += 1
+                    if grid[row - 1][col] == 0:
+                        x += 1
+                    if grid[row + 1][col] == 0:
+                        x += 1
+    return x + y
