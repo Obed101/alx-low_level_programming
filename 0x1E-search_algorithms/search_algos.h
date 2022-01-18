95a24ed8d7fbf9bdb5205091e3bf0da8ef13aef4
Obed101 <obedamoako92@gmail.com>
2022-01-18 10:21:44 +0000

    Header_file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SORTING_ALGOS_
+#define SORTING_ALGOS
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int binary_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+#endif
