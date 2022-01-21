1ec9a4e02048fbd0b9e7a5f7b42889662f99ff0d
Obed101 <obedamoako92@gmail.com>
2022-01-21 17:14:03 +0000

    handling NULL

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -34,21 +34,24 @@ int binary_search(int *array, size_t size, int value)
 
 	left = 0;
 	right = size - 1;
-	while (left <= right)
+	if (value)
 	{
-		mid = (left + right) / 2;
-		print(array, left, right);
-		if (array[mid] == value)
+		while (left <= right)
 		{
-			return (mid);
-		}
-		else if (array[mid] < value)
-		{
-			left = mid + 1;
-		}
-		else
-		{
-			right = mid - 1;
+			mid = (left + right) / 2;
+			print(array, left, right);
+			if (array[mid] == value)
+			{
+				return (mid);
+			}
+			else if (array[mid] < value)
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid - 1;
+			}
 		}
 	}
 	return (-1);
